574c843001067b461c8996decff9478e527ed6a5
guima <gui.lima30@gmail.com>
2018-01-05 10:08:59 -0200

    fix(landscape): Add verification if photos property exist

--- src/components/landscape.js
+++ src/components/landscape.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Landscape = ({ collection }) => {
-  if (collection.length === 0) return <div>No image</div>;
+  if (!collection && collection.length === 0) return <div>No image</div>;
   const images = collection.map((image, index) => {
     return <img src={image} key={index} />;
   });
