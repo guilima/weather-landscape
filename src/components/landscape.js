755c276a50af89c994874115a91a68d0044c5610
guilima <gui.lima30@gmail.com>
2018-01-07 15:16:11 -0200

    fix(landscape): Adjust conditional check on photos array

--- src/components/landscape.js
+++ src/components/landscape.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Landscape = ({ collection }) => {
-  if (!collection && collection.length === 0) return <div>No image</div>;
-  const images = collection.map((image, index) => {
-    return <img src={image} key={index} />;
-  });
+  if (!Array.isArray(collection) || collection.length === 0)
+    return <div>No image</div>;
+  const images = collection.map((image, index) => (
+    <img src={image} key={index} />
+  ));
   return <div className="video-list media">{images}</div>;
 };
 
