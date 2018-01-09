d67f2d8b30dc82042df3c47f54d11912874044f0
guilima <gui.lima30@gmail.com>
2018-01-08 22:15:20 -0200

    feat(app): Clean styles and html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Autocomplete from "./components/autocomplete";
 import Landscape from "./components/landscape";
@@ -16,10 +15,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Autocomplete
           setCollection={(images, address) =>
             this.setState({
@@ -28,9 +23,6 @@ class App extends Component {
             })
           }
         />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Landscape collection={this.state.images} city={this.state.address} />
       </div>
     );
