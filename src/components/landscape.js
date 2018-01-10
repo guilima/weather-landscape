44ddc28c7faafebffec621bee31f132296529937
guima <gui.lima30@gmail.com>
2018-01-10 16:51:13 -0200

    refactor(landscape): Change conditional check on collection assuming always as array

--- src/components/landscape.js
+++ src/components/landscape.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Landscape = ({ collection, city }) => {
-  if (!Array.isArray(collection) || collection.length === 0) return <div />;
+  if (collection.length === 0) return <div />;
   const images = collection.map((image, index) => (
     <img src={image} key={index} alt={"Locate in " + city} />
   ));
